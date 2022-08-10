a1f1666fd3f058d1a22f8cb3fa12885b1f7d0032
babanejad-m <m.b.sarabi@gmail.com>
2022-08-10 12:54:54 +0430

    chang dictation

--- src/Package/Main.java
+++ src/Package/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Person t2 = new Teacher("Maryam", "Bagheri", 38, 1008);
         Person S1 = new Student("Hamidreza", "Kamani", 23, 25);
         Person s2 = new Student("Arezo", "kabiri", 33, 26);
-        //درکلاس های abstract اجازه تعریف new object را نداریم پس آبجکت زیل خلاف قواعد بوده و میباید غیرفعال گردد
+        //درکلاس های abstract اجازه تعریف new object را نداریم پس آبجکت ذیل خلاف قواعد بوده و میباید غیرفعال گردد
         //Person p1 = new Person("Mehdi", "Abasi", 45);
         S1.printPersonInfo();
         S1.print();
